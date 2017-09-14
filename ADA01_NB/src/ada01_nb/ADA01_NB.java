4f548e5558746ee334d05585bdd7daddd219d9c6
Jacobo Gerardo González León <jgonzalezl1007@alumno.ipn.mx>
2017-09-14 15:01:08 -0500

    Test commit 2

--- ADA01_NB/src/ada01_nb/ADA01_NB.java
+++ ADA01_NB/src/ada01_nb/ADA01_NB.java
@@ -16,7 +16,7 @@ public class ADA01_NB {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hola mundo");
+        System.out.println("Hola no más");
     }
     
 }
