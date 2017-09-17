b55151d853e984ab882b52573185e974f5ab074d
Jacobo Gerardo González León <jgonzalezl1007@alumno.ipn.mx>
2017-09-17 14:46:29 -0500

    Algoritmo Erdös y Rényi completo sin gexf

--- ADA01_NB/src/ada01_nb/ADA01_NB.java
+++ ADA01_NB/src/ada01_nb/ADA01_NB.java
@@ -1,20 +1,20 @@
 package ada01_nb;
 
-import java.io.File;
-import java.io.IOException;
+//import java.io.File;
+//import java.io.IOException;
 import java.util.HashMap;
 
+/*
 import org.gephi.graph.api.DirectedGraph;
 import org.gephi.graph.api.Edge;
 import org.gephi.graph.api.GraphController;
 import org.gephi.graph.api.GraphModel;
 import org.gephi.graph.api.Node;
-import org.gephi.graph.api.UndirectedGraph;
 import org.gephi.io.exporter.api.ExportController;
 import org.gephi.project.api.ProjectController;
 import org.gephi.project.api.Workspace;
 import org.openide.util.Lookup;
-
+*/
 
 public class ADA01_NB {
     
@@ -22,211 +22,159 @@ public class ADA01_NB {
         System.out.println("Graphs");
         
         //Modelo G(n,m) de Erdös y Rényi
-        erdos_renyi(100, 30, true);
-        
-        //Graph toolkit API
-        //test_graphs();
-        
+        erdos_renyi(10, 30, true, false);
 
-    }
-
-    public static int volado(int min, int max){
-        return (int) Math.floor(Math.random()*(max-min+1)+(min));
     }
 
     /*
-        Modelo G(n,m) de Erdös y Rényi:
-        1. crear n vértices
-        2. elegir uniformemente al azar m distintos pares de distintos vértices
+    Modelo G(n,m) de Erdös y Rényi:
+    1. crear n vértices
+    2. elegir uniformemente al azar m distintos pares de distintos vértices
     */
 
-    public static void erdos_renyi(int n, int m, boolean dirigido){
+    public static void erdos_renyi(int n, int m, boolean dirigido, boolean auto){
+        //Repositorio de nodos
         HashMap hashMap_n = new HashMap();
+        
+        //Repositorio de aristas
         HashMap hashMap_m = new HashMap();
-        int aristas_totales = 0, aristas = 0;
-        
-        /*
-            Es necesario crear un proyecto para usar las características del kit de 
-            herramientas. La creación de un nuevo proyecto también crea un espacio 
-            de trabajo.
-        */
-        
-        //Init a project - and therefore a workspace
-        ProjectController pc_erdos_renyi = Lookup.getDefault().lookup(ProjectController.class);
-        pc_erdos_renyi.newProject();
-        Workspace ws_erdos_renyi = pc_erdos_renyi.getCurrentWorkspace();
-        
-        //Get a graph model - it exists because we have a workspace
-        GraphModel gm_erdos_renyi = Lookup.getDefault().lookup(GraphController.class).getGraphModel(ws_erdos_renyi);
-        
-        //Append as a Undirected Graph
-        DirectedGraph directedGraph = gm_erdos_renyi.getDirectedGraph();
-        
-        //Gnerar n vértices
+                     
+        //Generar n nodos empezando por el 1
         for(int v = 1; v <= n; v++){
             boolean respuesta = crear_nodo(v, hashMap_n);
-            
-            //Crear nodos
-            if(respuesta == true){
-                String nodo = Integer.toString(v);
-                
-                //Node[] neighbors = gm_erdos_renyi.factory().newNode(v);
-                Node n_nodo = gm_erdos_renyi.factory().newNode(nodo);
-                n_nodo.setLabel("Nodo " + nodo);
-                
-                //Append
-                directedGraph.addNode(n_nodo); 
-               
-            }
         }
+        
+        //Inicialización de aristas
+        int aristas_totales = 0, aristas = 1;
         
         //Generar m aristas
         while(aristas_totales != m){
-            //elegir un nodo al azar
-            int nodo_al_azar_1 = volado(1, n);
-            int nodo_al_azar_2 = volado(1, n);
-                        
-            //System.out.println("Se eligió al azar el nodo " + nodo_al_azar_1);
-            //System.out.println("Se eligió al azar el nodo " + nodo_al_azar_2);
+            //Elegir un nodo origen al azar
+            int nodo_origen = volado(1, n);
+            
+            System.out.println("Se eligió al azar el nodo origen: " + nodo_origen);
+            
+            //Checar si existe el nodo origen
+            if(hashMap_n.containsKey(nodo_origen)){
+                System.out.println("SÍ existe el nodo (key) " + nodo_origen); 
+            }
 
-            boolean respuesta = crear_arista(nodo_al_azar_1, nodo_al_azar_2, aristas, hashMap_m);
-
+            //Elegir un nodo destino al azar
+            int nodo_destino = volado(1, n);
+            
+            System.out.println("Se eligió al azar el nodo destino: " + nodo_destino);
+            
+            //Checar si existe el nodo destino
+            if(hashMap_n.containsKey(nodo_destino)){
+                System.out.println("SÍ existe el nodo (key) " + nodo_destino); 
+            }
+            
+            
+            //Se intenta crear el arista
+            System.out.println("Se intenta crear el arista " + nodo_origen + " + " + nodo_destino);
+            boolean respuesta = crear_arista(nodo_origen, nodo_destino, aristas, hashMap_m, dirigido, auto);
+            
+            //Si se creó el arista, continua 
             if(respuesta){
                 aristas++;
                 aristas_totales++;
-                
-                Node n_nodo_01 = gm_erdos_renyi.factory().newNode("0"); 
-                Node n_nodo_02 = gm_erdos_renyi.factory().newNode("0");
-                
-                
-                //Iterate over nodes
-                for (Node n_nodo_temp : directedGraph.getNodes()) {
-                    if(Integer.parseInt(n_nodo_temp.getId().toString()) == nodo_al_azar_1){
-                        n_nodo_01 = directedGraph.getNode(n_nodo_temp.getId());
-                        
-                        System.out.println("Nodo al azar: " + nodo_al_azar_1);
-                        System.out.println("Nuevo nodo => label: " + n_nodo_temp.getLabel() + " id: " + n_nodo_temp.getId());
-                        
-                        //break;
-                    }else if(Integer.parseInt(n_nodo_temp.getId().toString()) == nodo_al_azar_2){
-                        n_nodo_02 = directedGraph.getNode(n_nodo_temp.getId());
-                        
-                        System.out.println("Nodo al azar: " + nodo_al_azar_2);
-                        System.out.println("Nuevo nodo => label: " + n_nodo_temp.getLabel() + " id: " + n_nodo_temp.getId());
-                        
-                        //break;
-                    }
-                }
-                
-                System.out.println("Arista: " + n_nodo_01.getId().toString() + " + " + n_nodo_02.getId().toString());
-                
-                //Create an edge - directed and weight 1
-                Edge e_edge = gm_erdos_renyi.factory().newEdge(n_nodo_01, n_nodo_02, false);
-                directedGraph.addEdge(e_edge);
-                
             }
-
         }
 
-        //leer todas las aristas
+        //Se recorre el repositorio de aristas
+        for(int i = 1; i <= hashMap_m.size(); i++){
+            System.out.println("Arista " + i + ": " + hashMap_m.get(i));
+        }
+        
+        //Se leen el tamaño de los repositorios de nodos y aristas
         System.out.println("Nodos: " + hashMap_n.size());
         System.out.println("Aristas: " + hashMap_m.size());
-
-        for(int i = 1; i <= hashMap_m.size(); i++){
-            //System.out.println("Arista " + i + ": " + hashMap_m.get(i));
-            System.out.println(hashMap_m.get(i));
-        }
-        
-        //Export full graph
-        ExportController exportController = Lookup.getDefault().lookup(ExportController.class);
-        
-        try{
-            exportController.exportFile(new File("text.gexf"), ws_erdos_renyi);
-            System.out.println("Se creó el archivo gexf");
-        }catch(IOException e){
-            System.out.println(e);
-        }
-
-
     }
-
+    
     public static boolean crear_nodo(int key, HashMap hashMap_n){
         boolean respuesta = false;
 
         // checa el hashmap
-        // si existe el key del nodo, entonces no lo agrega al hashmap
-        if(hashMap_n.containsValue(key)){
+        // Si ya existe el nodo, entonces NO lo agrega al repositorio
+        if(hashMap_n.containsKey(key)){
+            System.out.println("NO se creó el nodo => (key) " + hashMap_n.get(key) + " (value) " + hashMap_n.values());
+            //Regresa falso
             respuesta = false;
-            //System.out.println("NO Se creó el nodo: " + hashMap_n.get(key));
-        // si no existe el key del nodo, entonces sí lo agrega al hashmap
-        }else if(!hashMap_n.containsValue(key)){
+            
+        // Si NO existe el nodo, entonces SÍ lo agrega al repositorio
+        }else if(!hashMap_n.containsKey(key)){
+            //Crea el nodo con key y value
+            String value = "Nodo: " + key;
+            hashMap_n.put(key, value);
+            System.out.println("Se creó el nodo => (key) " + key + " (value) " + hashMap_n.get(key));
+            
+            //Regresa verdadero
             respuesta = true;
-            hashMap_n.put(key, key);
-            //System.out.println("Se creó el nodo: " + hashMap_n.get(key));
         }
 
         return respuesta;
     }
 
-    public static boolean crear_arista(int key_nodo_u, int key_nodo_v, int key, HashMap hashMap_m){
-        boolean respuesta = false;
-        String arista = key_nodo_u + "--" + key_nodo_v;
-
-        // checa el hashmap
-        // si existe el key de la arista no lo agrega al hashmap
-        if(hashMap_m.containsValue(arista)){
-            respuesta = false;
-
-        // si no existe el key de la arista entonces sí lo agrega al hashmap
-        }else if(!hashMap_m.containsValue(arista)){
-            hashMap_m.put(key, arista);
-            //System.out.println("Se creó la arista: " + hashMap_m.get(key));
-            respuesta = true;
+    public static boolean crear_arista(int key_nodo_u, int key_nodo_v, int key, HashMap hashMap_m, boolean dirigido, boolean auto){
+        boolean respuesta = false, validacion = false;
+        String arista = null;
+        
+        //Si dirigido es verdadero, el grafo es dirigido
+        if(dirigido == true){
+            arista = key_nodo_u + "->" + key_nodo_v;
+            
+        //Si dirigido es falso, el grafo es no dirigido
+        }else if(dirigido == false){
+            arista = key_nodo_u + "--" + key_nodo_v;
         }
+              
+        System.out.println("Validando el arista: " + arista);
+                
+        //Si pueden existir autociclos, u y v pueden ser distintos o iguales
+        if(auto == true){
+            if(key_nodo_u == key_nodo_v || key_nodo_u != key_nodo_v){
+                validacion = true;
+            }
+            
+        //Si no pueden existir autociclos, u y v no pueden ser iguales
+        }else if(auto == false ){
+            if(key_nodo_u == key_nodo_v){
+                System.out.println("No se puede crear el arista: " + arista);
+                validacion = false;
+            }else if(key_nodo_u != key_nodo_v){
+                validacion = true;
+            }
+        }
+        
+        if(validacion == true){
+            // checa el hashmap
+            // Si existe el arista, NO lo agrega al repositorio
+            if(hashMap_m.containsValue(arista)){
+                System.out.println("Ya existe el arista: " + arista);
 
+                //Regresa falso
+                respuesta = false;
+
+            // Si no existe el arista, entonces SÍ lo agrega al repositorio
+            }else if(!hashMap_m.containsValue(arista)){
+                //Crea el arista con key y value
+                hashMap_m.put(key, arista);
+
+                System.out.println("Se creó el arista: " + hashMap_m.get(key));
+
+                //Regresa verdadero
+                respuesta = true;
+            }
+        }
+                           
+       
         return respuesta;
+    }    
+
+    public static int volado(int min, int max){
+        return (int) Math.floor(Math.random()*(max-min+1)+(min));
     }
     
-    public static void test_graphs() {
-        /*
-        Es necesario crear un proyecto para usar las características del kit de 
-        herramientas. La creación de un nuevo proyecto también crea un espacio 
-        de trabajo.
-        */
-        
-        //Init a project - and therefore a workspace
-        ProjectController projectController = Lookup.getDefault().lookup(ProjectController.class);
-        projectController.newProject();
-        
-        Workspace workspace = projectController.getCurrentWorkspace();
-        
-        //Create two nodes
-        GraphModel graphModel = Lookup.getDefault().lookup(GraphController.class).getGraphModel(workspace);
-        
-        Node n0 = graphModel.factory().newNode("n0");
-        n0.setLabel("Nodo n0");
-        
-        Node n1 = graphModel.factory().newNode("n1");
-        n1.setLabel("Nodo 01");
-        
-        //Create an edge - directed and weight 1
-        Edge e1 = graphModel.factory().newEdge(n0,n1, 1, true);
-        
-        //Append as a Directed Graph
-        DirectedGraph directedGraph = graphModel.getDirectedGraph();
-        directedGraph.addNode(n0);
-        directedGraph.addNode(n1);
-        directedGraph.addEdge(e1);
-        
-        //Export full graph
-        ExportController exportController = Lookup.getDefault().lookup(ExportController.class);
-        
-        try{
-            exportController.exportFile(new File("text.gexf"), workspace);
-            System.out.println("Se creó el archivo gexf");
-        }catch(IOException e){
-            System.out.println(e);
-        }
-            
-    }
+    
+    
 }
